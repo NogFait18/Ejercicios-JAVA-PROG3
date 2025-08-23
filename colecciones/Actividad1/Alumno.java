6a4580b02c3236f0f281d18ae257b80022468cf3
Fausto Chirino <faustetichirino@gmail.com>
2025-08-23 18:11:05 -0300

    subiendo actividad

---
+++ colecciones/Actividad1/Alumno.java
@@ -0,0 +1,22 @@
+package colecciones.Actividad1;
+
+public class Alumno {
+    public String nombre;
+    public int nota;
+
+    public Alumno(String nombre,int nota ) {
+        this.nombre = nombre;
+        this.nota = nota;
+    }
+
+    public Alumno() {
+    }
+
+    @Override
+    public String toString() {
+        return
+                "nombre='" + nombre + '\'' +
+                ", nota='" + nota + '\'' +
+                '}';
+    }
+}
---
+++ colecciones/Actividad1/Main.java
@@ -0,0 +1,21 @@
+package colecciones.Actividad1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main (String[]args){
+        List<Alumno> list = new ArrayList<>();
+
+        Alumno a1 = new Alumno("Lautaro",10);
+        Alumno a2 = new Alumno("Luis",7);
+        Alumno a3 = new Alumno("Miguel",9);
+
+        list.add(a1);
+        list.add(a2);
+        list.add(a3);
+
+        list.forEach(alumno -> System.out.println("Alumno: "+ alumno));
+
+    }
+}
